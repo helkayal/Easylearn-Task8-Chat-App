a8c5483fdade38419e0ed1976b848787d4da4e31
Hoda Elkayal <helkayal169@gmail.com>
2025-11-24 00:54:43 +0200

    modify app_colors

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -5,7 +5,9 @@ abstract class AppColors {
   static const Color backgroundColor = Color(0xFFFFFFFF);
   static const Color blackColor = Color(0xFF000000);
   static const Color whiteColor = Color(0xFFFFFFFF);
-  static const Color greyColor = Color(0xFFe7e7f1);
+  static const Color lightGreyColor = Color(0xFFe7e7f1);
+  static const Color greyColor = Color(0xFFB5B5BB);
   static const Color darkGreyColor = Color(0xFF47424b);
   static const Color blueColor = Color(0xFF448afe);
+  static const Color greenColor = Color(0xFF63a565);
 }
