f38b569b2cfc7149b52fa1f458027fcc24434a0a
Hoda Elkayal <helkayal169@gmail.com>
2025-11-23 23:08:05 +0200

    add app_strings to the app

--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -1,1 +1,6 @@
-abstract class AppStrings {}
+abstract class AppStrings {
+  static const String appName = 'Chat App';
+  static const String welcomeTo = 'Welcome To ';
+  static const String chats = 'Chats';
+  static const String search = 'Search';
+}
