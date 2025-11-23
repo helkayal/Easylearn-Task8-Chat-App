65ecee41f7e4e27cf95097f253834318d17be27c
Hoda Elkayal <helkayal169@gmail.com>
2025-11-24 00:35:02 +0200

    home screen appBar

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:task8_chat_app/core/theme/app_colors.dart';
+import 'package:task8_chat_app/features/home/presentation/views/home_screen_body.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,8 +8,24 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Screen')),
-      body: const Center(child: Text('Welcome to the Home Screen!')),
+      appBar: AppBar(
+        centerTitle: false,
+        title: Row(
+          spacing: 10,
+          children: [
+            CircleAvatar(
+              backgroundColor: AppColors.blueColor,
+              radius: 25,
+              child: CircleAvatar(
+                radius: 21,
+                backgroundImage: AssetImage("assets/images/profile.png"),
+              ),
+            ),
+            const Text('Hoda Elkayal'),
+          ],
+        ),
+      ),
+      body: const HomeScreenBody(),
     );
   }
 }
